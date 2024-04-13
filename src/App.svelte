be0c781c99321f9510a85cb5b723711ee57964a5
MistaOmega <jack.nash6@gmail.com>
2024-04-13 22:47:03 +0100

    Initial title, added vite folder to gitignore

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-import {Avatar} from "@skeletonlabs/skeleton";
+import {LightSwitch} from "@skeletonlabs/skeleton";
 </script>
 
-<main>
-    <Avatar src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" />
-</main>
+<main class="dark:mocha latte h-full w-full bg-base flex flex-1 items-center justify-center">
+    <header class="text-text py-20 transform -translate-x-80">
+        <h1 class="text-5xl font-bold">Hello there!</h1>
+        <p class="text-8xl">I'm <span class="text-sapphire font-bold">Jack.</span></p>
+        <p class="text-2xl mt-4">Full-Stack Developer, Technophile, Arch User </p>
+        <a href="about" class="text-2xl mt-4">Learn more about me</a>
+    </header>
 
+    <LightSwitch class="absolute top-0 right-0 m-4" />
+</main>
---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+f
+<div class="dark:mocha">
+    <p>About Me</p>
+</div>
